e64617fe5b28503e6a83c9161ef36b1a9a164b92
PedroManuel-medac <liebanapedromanuel@gmail.com>
2025-07-10 09:19:05 +0200

    Actualización del proyecto

--- js/JavaScript.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.getElementById('navToggle');
-  const menu = document.getElementById('mobileMenu');
-  toggle.addEventListener('click', () => {
-    menu.classList.toggle('open');
-  });
-});
