621018ecf513d6cd424c3ded4f0bc1ed7553f321
PedroManuel-medac <liebanapedromanuel@gmail.com>
2025-07-11 11:11:08 +0200

    Apartado "Galería"
    Desarrollo del apartado galería. Cuadricula de 3x2

--- JavaScript.js
+++ JavaScript.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+  console.log("JS cargado correctamente");
+
   const botonMenu = document.querySelector('.menu-movil');
   const menuDesplegable = document.getElementById('menuMovil');
 
   if (botonMenu && menuDesplegable) {
     botonMenu.addEventListener('click', () => {
       menuDesplegable.classList.toggle('activo');
+      console.log("Menú desplegable toggled");
     });
   } else {
     console.error("Botón o menú no encontrados");
   }
 });
-
